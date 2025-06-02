7b56aac8607856d53666f95a15b3c4510ec1b4d4
ShodanMaster <akshaykrishnan922@gmail.com>
2025-06-02 22:45:23 +0530

    License Plate Validation

--- app/Http/Requests/ScanInRequest.php
+++ app/Http/Requests/ScanInRequest.php
@@ -17,11 +17,16 @@ class ScanInRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'vehicleNumber' => 'required|string',
+            'vehicleNumber' => [
+                'required',
+                'string',
+                'regex:/^[A-Z]{2}[0-9]{1,2}[A-Z]{0,2}[0-9]{4}$/i',
+            ],
             'vehicleId' => 'required|integer|exists:vehicles,id',
             'locationId' => 'required|integer|exists:locations,id',
         ];
     }
+
 
     public function withValidator($validator): void
     {
@@ -48,4 +53,12 @@ class ScanInRequest extends FormRequest
             }
         });
     }
+
+    public function messages(): array
+    {
+        return [
+            'vehicleNumber.regex' => 'The vehicle number format is invalid. Example: KL12AB1234',
+        ];
+    }
+
 }
