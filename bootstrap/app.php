a3e33c3a6ccd8cc479f12cd2471aae31ea834430
ShodanMaster <akshaykrishnan922@gmail.com>
2025-06-05 15:15:13 +0530

    Delete 1 day old image and also from Database

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,13 @@
 
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\AdminWorkerMiddleware;
+use App\Models\QrCode;
+use Carbon\Carbon;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Storage;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +21,19 @@ return Application::configure(basePath: dirname(__DIR__))
                 'admin' => AdminMiddleware::class,
                 'admin_worker' => AdminWorkerMiddleware::class,
             ]);
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(function () {
+            $qrCodes = QrCode::where('created_at', '<', Carbon::yesterday())->get();
+
+            foreach ($qrCodes as $qrCode) {
+                $filePath = str_replace('/storage', '', $qrCode->path);
+
+                Storage::disk('public')->delete($filePath);
+                $qrCode->delete();
+            }
+
+        })->daily();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
